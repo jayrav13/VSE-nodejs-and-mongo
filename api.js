8839a91a6910c4b4575d3f7418722ef4cd0c9719
Jay Ravaliya <jayrav13@gmail.com>
2015-10-05 02:16:01 +0000

    Completed

--- api.js
+++ api.js
@@ -11,12 +11,34 @@ app.get('/', function (req, res) {
 	res.send('Hello World!');
 });
 
+app.get('/profile', function(req, res) {
+
+  var user = db.collection('allUsers').find({'token':req.param('token')});
+  var found = 0
+
+  user.each(function(err, doc) {
+
+    if(doc != null) {
+      found = 1;
+      res.status(200).json(doc);
+    }
+    else if(doc == null && found == 0) {
+      res.status(400).send({'fail':'User not found.'});
+    }
+
+  });
+
+});
+
 app.get('/login', function(req, res) {
 	
 	var token = randtoken.generate(32);
 	var found = 0;
 	var user = db.collection('allUsers').find({'username':req.param('username'), 'password':req.param('password')});
-	
+
+  console.log(req.param('username'));
+  console.log(req.param('password'));
+
 	user.each(function(err, doc) {
 		if(doc != null) {
 			db.collection('allUsers').update(doc, {$set: {'token':token}});
@@ -75,9 +97,46 @@ app.get('/buy', function(req, res) {
 	
 	user.each(function(err, doc) {
 		if(doc != null) {
-			found = 1;
-			db.collection('allUsers').update({'token':req.param('token')}, {$inc: {}});			
-			res.status(200).send({'success':'Made it!'});
+			found = 1; 
+
+    function constructJson(ticker, shares){
+      var jsonObj = {};
+      jsonObj['stocks.' + ticker + '.shares'] = parseInt(shares);
+      jsonObj['money'] = -1 * parseInt(parseInt(req.param('shares')) * parseFloat(req.param('price')));
+      console.log(jsonObj['money']);
+      return jsonObj;
+     }
+
+      db.collection('allUsers').update(doc, {$inc: constructJson(req.param('ticker'), req.param('shares'))});
+
+      res.status(200).send({'success':{'ticker':req.param('ticker'), 'shares':req.param('shares')}});
+		} 
+		else if(doc == null && found == 0) {
+			res.status(400).send({'fail':'Token error.'});
+		}
+	});
+
+});
+
+app.get('/sell', function(req, res) {
+	
+	var found = 0;
+	var user = db.collection('allUsers').find({'token':req.param('token')});
+	
+	user.each(function(err, doc) {
+		if(doc != null) {
+			found = 1; 
+
+    function constructJson(ticker, shares){
+      var jsonObj = {};
+      jsonObj['stocks.' + ticker + '.shares'] = -1 * parseInt(shares);
+      jsonObj['money'] = parseInt(parseInt(req.param('shares')) * parseFloat(req.param('price')));
+      console.log(jsonObj['money']);
+      return jsonObj;
+     }
+      db.collection('allUsers').update(doc, {$inc: constructJson(req.param('ticker'), req.param('shares'))});
+
+      res.status(200).send({'success':{'ticker':-1 * req.param('ticker'), 'shares':-1 * req.param('shares')}});
 		} 
 		else if(doc == null && found == 0) {
 			res.status(400).send({'fail':'Token error.'});
